1afe1726d480ecd774adb593bc023842ed55dabd
shubh-7028 <sangita333wakil@gmail.com>
2023-09-18 18:12:54 +0530

    rebase

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,10 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
+<<<<<<< HEAD
         'body': json.dumps('Hello from Lambda!!!, git changes 2nd time')
         'body': json.dumps('Hello from Lambda!!!, dev1 changes')
+=======
+        'body': json.dumps('Hello from Lambda!!!, master changes')
+>>>>>>> 4e978ae (master changes)
     }
